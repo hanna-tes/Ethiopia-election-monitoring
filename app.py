b6f607b3abdefafa759d17ca0f0641a7b982d8e1
hanna <82633682+hanna-tes@users.noreply.github.com>
2026-04-20 13:05:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ CFA_LOGO_URL = "https://opportunities.codeforafrica.org/wp-content/uploads/sites
 MELTWATER_URL = "https://raw.githubusercontent.com/hanna-tes/Ethiopia-election-monitoring/refs/heads/main/MeltwaterEthiopiaMar8.csv?token=GHSAT0AAAAAADRDAPFL7BRYK6DIX4HFMBUQ2PF6EBQ"
 CIVICSIGNALS_URL = "https://raw.githubusercontent.com/hanna-tes/Ethiopia-election-monitoring/refs/heads/main/EthiopiaCivicsignalMar8.csv?token=GHSAT0AAAAAADRDAPFLHLGT4462WCZI6VWC2PF6APQ"
 TIKTOK_URL = "https://raw.githubusercontent.com/hanna-tes/Disinfo_monitoring_RadarSystem/refs/heads/main/TikTok_Oct_Nov.csv"
-OPENMEASURES_URL = "https://raw.githubusercontent.com/hanna-tes/Disinfo_monitoring_RadarSystem/refs/heads/main/open-measures-data%20(4).csv"
+OPENMEASURES_URL = "https://raw.githubusercontent.com/hanna-tes/Ethiopia-election-monitoring/refs/heads/main/EthiopiaopenmeasuresApri17.csv?token=GHSAT0AAAAAADRDAPFLUMOFJ7I22WQ7EJ6S2PF7JPA"
 ORIGINAL_POSTS_URL = "https://raw.githubusercontent.com/hanna-tes/Disinfo_monitoring_RadarSystem/refs/heads/main/Co%CC%82te_dIvoire_OR_Ivory_Coast_OR_Abidjan_OR_Ivoirien%20-%20Jan%2029%2C%202026%20-%205%2021%2000%20PM.csv"
 
 # --- Helper Functions ---
